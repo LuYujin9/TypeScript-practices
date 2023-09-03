c94663437ab9451e02db9f769e21e86904a8123e
LuYujin9 <wuwujane@hotmail.com>
2023-09-03 16:49:47 +0200

    state type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -5,7 +5,7 @@ type AuthUser = {
   email: string;
 };
 
-const User = () => {
+/* const User = () => {
   const [user, setUser] = useState<AuthUser | null>(null); //可能为两种类型
   const handleLogin = () => {
     setUser({
@@ -25,6 +25,24 @@ const User = () => {
     </div>
   );
 };
+//此时使用useState 的props时,要加?,因为值有可能是null,就会报错 */
 
-//使用props时,要加?,因为值有可能是null,就会报错
+//当state不会重新设置为null时,可以这样写,不需要check user的value
+const User = () => {
+  const [user, setUser] = useState<AuthUser>({} as AuthUser); //use type Assertion, 目的: 让TS认为空的object是一个authUser
+  const handleLogin = () => {
+    setUser({
+      name: "Jane",
+      email: "jane@email.com",
+    });
+  }; //此时user已经被设定为null,不能为string了
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
+    </div>
+  );
+};
+
 export default User;
